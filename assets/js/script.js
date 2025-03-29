4646316b823e72cdfa0fc958be9d17a1fe5f4936
Janmenque <janmenque75@gmail.com>
2025-03-29 22:42:53 +0800

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend development", "backend development", "web designing", "android development", "web development"],
+    strings: ["frontend developer", "backend developer", "web designing", "android developer", "web developer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
